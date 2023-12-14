456f2f9c000df9b52de9b2ef687746a4e1b66ea5
Batoum samir <112777869+batoum0-code@users.noreply.github.com>
2023-12-14 18:24:45 +0100

    Update NavBar.jsx
    changing the home page h2 or title

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className='nav-bar'>
-        <Link to={'/'} className='logo'>Home</Link>
+        <Link to={'/'} className='logo'> this the Home page </Link>
         <ul className='nav-items'>
             <li>Products</li>
             <li>About Us</li>
